7ceb81d063b6b34015a50934d9e814be95934b4b
Peter Solnica <peter@solnica.online>
2023-03-14 08:29:08 +0100

    Initial commit

---
+++ app/action.rb
@@ -0,0 +1,9 @@
+# auto_register: false
+# frozen_string_literal: true
+
+require "hanami/action"
+
+module DockerDemo
+  class Action < Hanami::Action
+  end
+end
---
+++ config/app.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "hanami"
+
+module DockerDemo
+  class App < Hanami::App
+  end
+end
